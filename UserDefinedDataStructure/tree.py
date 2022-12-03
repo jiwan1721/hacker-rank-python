a696050ab84a3cae160dc4c12332c3520141862f
Jiwan <chandjiwn9@gmail.com>
2022-12-03 13:19:07 +0545

    traversal algorithm

--- UserDefinedDataStructure/tree.py
+++ UserDefinedDataStructure/tree.py
@@ -8,4 +8,33 @@ root = Node('A')
 root.childleft = Node('B')
 root.childright = Node('C')
 root.childleft.childleft = Node('D')
+root.childleft.childright= Node('E')
 
+
+#in order traversal
+def InOrd(root):
+    if root:
+        InOrd(root.childleft)
+        print(root.nodedata)
+        InOrd(root.childright)
+
+InOrd(root)
+
+
+# pre order traversals
+
+def preOrd(root):
+    if root:
+        print("-----",root.nodedata)
+        preOrd(root.childleft)
+        preOrd(root.childright)
+
+preOrd(root)
+
+def PostOrd(root):
+    print("   ",root)
+    if root:
+        PostOrd(root.childleft)
+        PostOrd(root.childright)
+        print("post----",root.nodedata)
+PostOrd(root)
